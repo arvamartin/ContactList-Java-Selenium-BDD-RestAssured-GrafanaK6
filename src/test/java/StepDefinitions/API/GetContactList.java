72f6a2e7bdf71a345821074209454081ee34962a
arvam <arvamartin02@gmail.com>
2025-04-13 14:50:30 +0200

    added GET api test

---
+++ src/test/java/StepDefinitions/API/GetContactList.java
@@ -0,0 +1,45 @@
+package StepDefinitions.API;
+
+import Utils.ConfigReader;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.Header;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class GetContactList {
+
+    private RequestSpecification httpRequest;
+    private Response response;
+    private final String authToken = ConfigReader.getValue("user","BearerAuth");
+
+    @Given("hit the url of contacts api endpoint")
+    public void hitTheContactsGetUrl() {
+        RestAssured.baseURI = "https://thinking-tester-contact-list.herokuapp.com";
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer " + authToken);
+        httpRequest = RestAssured.given().headers(headers);
+    }
+
+    @When("pass the url of contacts in the request")
+    public void passTheUrlOfProductsInTheRequest() {
+        response = httpRequest.get("/contacts");
+    }
+
+
+    @Then("receive the {int} response code")
+    public void receiveTheResponseCode(int statusCode) {
+        int actualStatusCode = response.getStatusCode();
+        System.out.println(actualStatusCode);
+        assertThat(actualStatusCode, is(statusCode));
+    }
+}
