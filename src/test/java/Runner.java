161742cb10c508e9f2c86066536fdedec892b0b1
arvam <arvamartin02@gmail.com>
2025-04-19 15:48:04 +0200

    added Runner

---
+++ src/test/java/Runner.java
@@ -0,0 +1,14 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/Features/API", "src/test/resources/Features/UI"},
+        glue = {"/StepDefinitions/API", "StepDefinitions/UI", "Utils"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        monochrome = true
+//        , tags = ""
+)
+public class Runner {
+}
