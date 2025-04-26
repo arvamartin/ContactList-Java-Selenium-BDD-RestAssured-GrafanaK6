28bebc7ca876a3faea62cc1d30bcdcb1f0476084
arvam <arvamartin02@gmail.com>
2025-04-26 10:38:44 +0200

    Refactored utils

--- src/test/java/Utils/JsonParser.java
+++ src/test/java/Utils/JsonParser.java
@@ -1,22 +1,39 @@
 package Utils;
 
 import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JsonParser {
+    private static Logger logger = (Logger) LogManager.getLogger(JsonParser.class);
 
-    public static Map<String, Object> jsonReader(String fileName) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        InputStream inputStream = JsonParser.class.getClassLoader().getResourceAsStream("Json/" + fileName);
+    public static Map<String, Object> jsonReader(String fileName) {
+        Map<String, Object> result;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream inputStream = JsonParser.class.getClassLoader().getResourceAsStream("Json/" + fileName);
 
-        if (inputStream == null) {
-            throw new IllegalArgumentException("Cannot find file: " + fileName);
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Cannot find file: " + fileName);
+            }
+
+            result = mapper.readValue(inputStream, Map.class);
+        } catch (IOException e) {
+            logger.warn("Error while reading JSON file: " + fileName + " - " + e.getMessage());
+            throw new RuntimeException("Failed to read JSON file: " + fileName, e);
+
+        } catch (Exception e) {
+            logger.warn("Unexpected error while reading JSON file: " + fileName + " - " + e.getMessage());
+            throw new RuntimeException("Unexpected error while reading JSON: " + fileName, e);
         }
-        return mapper.readValue(inputStream, Map.class);
+        return result;
     }
+
 
 
 }
---
+++ src/test/java/Utils/UpdateUtil.java
@@ -0,0 +1,33 @@
+package Utils;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import java.util.Map;
+
+public class UpdateUtil {
+
+    private static final String BASE_URI = "https://thinking-tester-contact-list.herokuapp.com";
+
+    public static Response performUpdate(String method, String param, Map<String, Object> headers, Map<String, Object> reqBody) {
+
+        RestAssured.baseURI = BASE_URI;
+
+        Response response = switch (method.toLowerCase()) {
+            case "put" -> RestAssured.given()
+                    .contentType("application/json")
+                    .headers(headers)
+                    .body(reqBody)
+                    .put(param);
+            case "patch" -> RestAssured.given()
+                    .contentType("application/json")
+                    .headers(headers)
+                    .body(reqBody)
+                    .patch(param);
+            default -> throw new IllegalArgumentException("Unsupported method: " + method);
+        };
+        System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
+        System.out.println(response.getBody().prettyPrint());
+
+        return response;
+    }
+}
