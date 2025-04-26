bbd7abfb68d370db42b1f2be07dc565c8e3fb0b4
arvam <arvamartin02@gmail.com>
2025-04-26 10:38:53 +0200

    Refactored

--- src/test/java/StepDefinitions/API/UpdateContacts.java
+++ src/test/java/StepDefinitions/API/UpdateContacts.java
@@ -1,54 +1,40 @@
 package StepDefinitions.API;
 
+import Utils.HeaderBuilder;
 import Utils.JsonParser;
+import Utils.UpdateUtil;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class UpdateContacts {
     private Response response;
-    Map<String, Object> reqBody;
-    Map<String, Object> headers;
-    private final String BASE_URI = "https://thinking-tester-contact-list.herokuapp.com";
+    private Map<String, Object> reqBody;
+
 
     @When("update contact from {string} in {string} put request")
     public void updateFullContact(String fileName, String param) throws IOException {
         reqBody = JsonParser.jsonReader(fileName);
-        String authToken = System.getProperty("authToken");
-        headers = new HashMap<>();
-        headers.put("Authorization", "Bearer " + authToken);
+        Map<String, Object> headers = HeaderBuilder.buildAuthHeaders();
+        Map<String, Object> reqBody = JsonParser.jsonReader(fileName);
 
-        RestAssured.baseURI = BASE_URI;
-        response = RestAssured.given()
-                .contentType("application/json")
-                .headers(headers)
-                .body(reqBody)
-                .put(param);
+        response = UpdateUtil.performUpdate("put", param, headers, reqBody);
 
         System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
         System.out.println(response.getBody().prettyPrint());
     }
 
     @When("update contact from {string} in {string} patch request")
-        public void updatePartiallyContact(String fileName, String param) throws IOException {
-            reqBody = JsonParser.jsonReader(fileName);
-            String authToken = System.getProperty("authToken");
-            headers = new HashMap<>();
-            headers.put("Authorization", "Bearer " + authToken);
+    public void updatePartiallyContact(String fileName, String param) throws IOException {
+        reqBody = JsonParser.jsonReader(fileName);
+        Map<String, Object> headers = HeaderBuilder.buildAuthHeaders();
 
-            RestAssured.baseURI = BASE_URI;
-            response = RestAssured.given()
-                    .contentType("application/json")
-                    .headers(headers)
-                    .body(reqBody)
-                    .patch(param);
+        response = UpdateUtil.performUpdate("patch", param, headers, reqBody);
 
-            System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
-            System.out.println(response.getBody().prettyPrint());
-        }
+        System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
+        System.out.println(response.getBody().prettyPrint());
+    }
 }
 
