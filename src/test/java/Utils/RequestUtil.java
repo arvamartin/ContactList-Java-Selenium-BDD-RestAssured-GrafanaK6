e68cb9a4f04888f23cacd6ad93000830f0982cdf
arvam <arvamartin02@gmail.com>
2025-04-27 11:12:24 +0200

    Refactored sendRequest method in RequestUtil.java

--- src/test/java/Utils/RequestUtil.java
+++ src/test/java/Utils/RequestUtil.java
@@ -2,6 +2,8 @@ package Utils;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import java.util.Map;
 
 public class RequestUtil {
@@ -11,26 +13,21 @@ public class RequestUtil {
     public static Response sendRequest(String method, String param, Map<String, Object> headers, Map<String, Object> reqBody) {
         RestAssured.baseURI = BASE_URI;
 
+        RequestSpecification httpRequest = RestAssured.given()
+                .contentType("application/json")
+                .body(reqBody);
+
+        if (headers != null) {
+            httpRequest.headers(headers);
+        }
+
         Response response = switch (method.toLowerCase()) {
-            case "put" -> RestAssured.given()
-                    .contentType("application/json")
-                    .headers(headers)
-                    .body(reqBody)
-                    .put(param);
-            case "patch" -> RestAssured.given()
-                    .contentType("application/json")
-                    .headers(headers)
-                    .body(reqBody)
-                    .patch(param);
-            case "post" -> RestAssured.given()
-                    .contentType("application/json")
-                    .headers(headers)
-                    .body(reqBody)
-                    .post(param);
+            case "put" -> httpRequest.put(param);
+            case "patch" -> httpRequest.patch(param);
+            case "post" -> httpRequest.post(param);
             default -> throw new IllegalArgumentException("Unsupported method: " + method);
         };
         System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
-        System.out.println(response.getBody().prettyPrint());
 
         return response;
     }
