3737b3302b19eecd8ee89b76492fe6f355c334cd
arvam <arvamartin02@gmail.com>
2025-04-30 21:49:40 +0200

    Refactored patch-put method

--- src/test/java/StepDefinitions/API/UpdateApiStep.java
+++ src/test/java/StepDefinitions/API/UpdateApiStep.java
@@ -8,29 +8,13 @@ import io.restassured.response.Response;
 import java.util.Map;
 
 public class UpdateApiStep {
-    private Response response;
-    private Map<String, Object> reqBody;
-    private Map<String, Object> headers;
 
+    @When("update contact from {string} in {string} {string} request")
+    public void updateFullContact(String fileName, String param, String request) {
+        Map<String, Object> reqBody = JsonParser.jsonReader(fileName);
+        Map<String, Object> headers = RequestUtil.buildAuthHeaders();
 
-    @When("update contact from {string} in {string} put request")
-    public void updateFullContact(String fileName, String param) {
-        reqBody = JsonParser.jsonReader(fileName);
-        headers = RequestUtil.buildAuthHeaders();
-        reqBody = JsonParser.jsonReader(fileName);
-
-        response = RequestUtil.sendRequest("put", param, headers, reqBody);
-        System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
-        response.getBody().prettyPrint();
-    }
-
-
-    @When("update contact from {string} in {string} patch request")
-    public void updatePartiallyContact(String fileName, String param) {
-        reqBody = JsonParser.jsonReader(fileName);
-        headers = RequestUtil.buildAuthHeaders();
-
-        response = RequestUtil.sendRequest("patch", param, headers, reqBody);
+        Response response = RequestUtil.sendRequest(request, param, headers, reqBody);
         System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
         response.getBody().prettyPrint();
     }
--- src/test/resources/Features/API/UPDATEApi.feature
+++ src/test/resources/Features/API/UPDATEApi.feature
@@ -3,7 +3,7 @@ Feature: Patch & Put - update
 
   Scenario Outline: verify the patch api for the contacts
     Given get user's auth token
-    And update contact from "PatchBody.json" in "/contacts/<contactID>" patch request
+    And update contact from "PatchBody.json" in "/contacts/<contactID>" "patch" request
     Then receive the 200 response code
     Examples:
       | contactID                |
@@ -12,7 +12,7 @@ Feature: Patch & Put - update
 
   Scenario Outline: verify the put api for the contacts
     Given get user's auth token
-    And update contact from "PutBody.json" in "/contacts/<contactID>" put request
+    And update contact from "PutBody.json" in "/contacts/<contactID>" "put" request
     Then receive the 200 response code
     Examples:
       | contactID                |
