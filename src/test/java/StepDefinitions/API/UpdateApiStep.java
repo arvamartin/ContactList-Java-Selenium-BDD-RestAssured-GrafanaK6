b9c235e87fe0c7cc67de3a9d89da18f526fdee00
arvam <arvamartin02@gmail.com>
2025-05-04 22:09:15 +0200

    Added try-catch and logger to UpdateApiStep.java

--- src/test/java/StepDefinitions/API/UpdateApiStep.java
+++ src/test/java/StepDefinitions/API/UpdateApiStep.java
@@ -4,19 +4,27 @@ import Utils.JsonParser;
 import Utils.RequestUtil;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
 public class UpdateApiStep {
+    Logger logger = LogManager.getLogger(getClass());
 
     @When("update contact from {string} in {string} {string} request")
     public void updateFullContact(String fileName, String param, String request) {
-        Map<String, Object> reqBody = JsonParser.jsonReader(fileName);
-        Map<String, Object> headers = RequestUtil.buildAuthHeaders();
+        try {
+            Map<String, Object> reqBody = JsonParser.jsonReader(fileName);
+            Map<String, Object> headers = RequestUtil.buildAuthHeaders();
 
-        Response response = RequestUtil.sendRequest(request, param, headers, reqBody);
-        System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
-        response.getBody().prettyPrint();
+            Response response = RequestUtil.sendRequest(request, param, headers, reqBody);
+            System.setProperty("actualStatusCode", String.valueOf(response.getStatusCode()));
+            response.getBody().prettyPrint();
+
+        } catch (Exception e) {
+            logger.warn("error: {}", e.getMessage(), e);
+        }
     }
 }
 
