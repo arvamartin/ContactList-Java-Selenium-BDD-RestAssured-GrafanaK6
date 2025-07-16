69d50074a538900bb4278fbdb5caff461870ad7c
arvam <arvamartin02@gmail.com>
2025-07-16 10:53:15 +0200

    Implemented takeScreenshot method to Hook.js in case if tests fail

--- src/test/java/Utils/Hook.java
+++ src/test/java/Utils/Hook.java
@@ -1,11 +1,44 @@
 package Utils;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.logging.log4j.Logger;
+
+import static Utils.Browser.getDriver;
+import static Utils.Browser.quitDriver;
+
 
 public class Hook {
+    private static Logger logger = LogManager.getLogger(Hook.class);
 
     @After
-    public void tearDown(){
-        Browser.quitDriver();
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            takeScreenShot(scenario.getName());
+        }
+        if (getDriver() != null) {
+            quitDriver();
+        }
+    }
+
+
+    private void takeScreenShot(String name) {
+        try {
+            File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+            File destFile = new File("screenshots/" + name + ".png");
+            FileUtils.copyFile(scrFile, destFile);
+            logger.info("Screenshot saved to: {}", destFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.warn("Problem with taking/saving screenshot");
+        }
     }
 }
